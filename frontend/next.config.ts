b221355171f59710da5e900a44604bc77ad67e57
Olivier Bitter <olibitter@gmail.com>
2026-06-07 10:10:43 +0200

    add dev origins

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  allowedDevOrigins: ["192.168.1.232"],
+  allowedDevOrigins: ["192.168.1.232", "192.168.1.5"],
 };
 
 export default nextConfig;
